962ffa5e6fbc1cccf3f57da7d190e90ced149d45
Artur Danilenko <danartur99@gmail.com>
2020-05-03 17:45:01 -0700

    Added speed slider in web + Yun - Server comm

--- html_input_output/html_input_output.ino
+++ html_input_output/html_input_output.ino
@@ -16,35 +16,28 @@ int val = 1;
 void setup() {
   Bridge.begin();
  // Console.begin();
-  // Listen for incoming connection only from localhost
-  // (no one from the external network could connect)
   server.listenOnLocalhost();
-  server.begin();
-   
-  // get the time that this sketch started:
-   
-  pinMode(led,OUTPUT); //initialize the digital pin (which controls the LED) as an output.
- // digitalWrite(led,1);
-// while(!Console);
- 
+  server.begin();  
 }
 
 void loop() { 
   int LED_choice;
- //Console.print("qwq");
   BridgeClient client = server.accept();  
- // Console.println(client);
-  delay(50);
-//  digitalWrite(led,1);
-//  delay(250);
-//  digitalWrite(led,0);
- // delay(250);
+  client.println("client accepted");
+  digitalWrite(led, HIGH);
+  delay(150);
+  client.println("delay 1 passed led should be low");
+  digitalWrite(led, LOW);
+  delay(150);
+  client.println("delay 2 passed led should be low");
   if (client) { 
     String command = client.readStringUntil('/'); //Read in the string up to the frist
     command.trim();        //kill whitespace
-    if (command == "temperature") {  //Check to see if the first part of the URL command 
-      client.print(" degrees C");      
+    if (command == "temperature") {  //Check to see if the first part of the URL command     
       LED_choice=client.parseInt();
+      //client.print("<br>Arduino says LED on.");
+      client.println(LED_choice);
+      client.println(hits);
       if (LED_choice == 1) { // the URL "/arduino/temperature/1" was sent 
           client.print("<br>Arduino says LED on."); //Turn the LED on by making the 
           digitalWrite(led, HIGH);
@@ -54,12 +47,12 @@ void loop() {
         digitalWrite(led, LOW); //Turn the LED off by making the voltage LOW
       }
     }
-     
+    client.println("commands executed");
      // Close connection and free resources.
     client.stop();
     client.flush();//discard any bytes that have been written to client but not 
     //yet read.
     hits++; //increment the "hits" counter by 1.
   }    
-  
+  client.println("if statement passed");
 } 
--- html_input_output/www/js/jsHere.js
+++ html_input_output/www/js/jsHere.js
@@ -2,24 +2,31 @@ $(function() {
 	getSensorvalue();
 });
 
+var oldcommand = "";
+
 function getSensorvalue() {
     //This function gets sensor values from Arduino AND sends out a request to turn LED on
 	//$('#sensor_content').load('/arduino/temperature/'); //send a request for temperature/humidity data
 	//$('#LED_content').load('/arduino/temperature/1');
 	var checked_option_radio = $('input[name=LEDCheck]:checked','#LED_Selection').val();
-	
-	 if (checked_option_radio =='off') //if the user selected for LED to be off, send request to Arduino
-	{
-		//$("#LED_content_test").html("LED is turned off");
-	 	$('#LED_content').load('/arduino/temperature/0');
-	 }
-	
-	  if (checked_option_radio =='on') //if the user selected for LED to be on, send request to Arduino
-	{
-		//$("#LED_content").html("LED is turned on");
-	 	$('#LED_content').load('/arduino/temperature/1');
-	 }
-
-	setTimeout("getSensorvalue()",1000); //Pole every one seconds.	
+	var slider = document.getElementById("myRange");
+	command = '/arduino/temperature/';
+	command = command + slider.value.toString();
+	if(oldcommand!=command){
+		if (checked_option_radio =='off') //if the user selected for LED to be off, send request to Arduino
+		{
+			//$("#LED_content_test").html("LED is turned off");
+			command = '/arduino/temperature/0';
+			$('#LED_content').load(command);
+		}
+		
+		if (checked_option_radio =='on') //if the user selected for LED to be on, send request to Arduino
+		{
+			//$("#LED_content").html("LED is turned on");
+			$('#LED_content').load(command);
+		}
+		oldcommand = command;
+	}
+	setTimeout("getSensorvalue()",500); //Pole every one seconds.	
 }
 
